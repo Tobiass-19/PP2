92da365cddb48429820889dff781c955895687a9
Tobias Leclercq <tobiasleclercq1@gmail.com>
2023-02-10 00:41:23 -0300

    Añadimos formulario (sin funcionalidades), faltan ajustar detalles //// Añadimos sección información para interactuar con el formulario //// Cambiamos colorización //// Corregimos algunos bugs

---
+++ src/js/form.js
@@ -0,0 +1,28 @@
+const button = document.querySelector(".contact-button");
+const form = document.querySelector(".contact-container");
+const emailContact = document.querySelector("emai-contact");
+
+
+// window.addEventListener("scroll", () => {
+//     console.log(`X: ${window.scrollX} Y: ${window.scrollY}`);
+// })
+
+// window.addEventListener("resize", () => {
+//     console.log("width: " + window.innerWidth);
+// })
+
+button.addEventListener('click', async ()=>{
+    
+    if(button.innerText == "¡Haga Click Aquí!") {
+        button.innerText = "Cerrar Formulario"  
+    } else {
+        button.innerText = "¡Haga Click Aquí!"
+    }
+    
+    form.classList.toggle("form-spread")
+    await setTimeout(() => {
+        form.classList.toggle("form-position")
+    }, 800);
+})
+
+
